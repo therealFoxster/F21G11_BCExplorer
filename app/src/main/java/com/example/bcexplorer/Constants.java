794b2fe8ae9587754f37ccbd908a100ea6996186
David Aste <78303927+davidaste@users.noreply.github.com>
2021-11-18 14:56:40 -0800

    The list items are added

---
+++ app/src/main/java/com/example/bcexplorer/Constants.java
@@ -0,0 +1,10 @@
+package com.example.bcexplorer;
+
+public class Constants {
+
+        public static final String WHITE_ROCK = "whiterock";
+        public static final String PARAMS = "params";
+        public static final String WHISTLER = "whistler";
+        public static final String VANCOUVER = "vancouver";
+    }
+
---
+++ app/src/main/java/com/example/bcexplorer/ListDetailActivity.java
@@ -0,0 +1,14 @@
+package com.example.bcexplorer;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListDetailActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_detail);
+    }
+}
